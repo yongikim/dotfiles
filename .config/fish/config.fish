50226d70d640122364339c6bc87af3699365815d
yongikim <yongi.kim08@gmail.com>
2020-02-23 16:05:26 +0900

    Add .config/fish

---
+++ .config/fish/config.fish
@@ -0,0 +1,21 @@
+set -x LANG ja_JP.UTF-8
+alias reload='source ~/.config/fish/config.fish'
+alias gst='git status'
+alias dsales='cd ~/projects/sales;docker-compose start sales redis;docker attach sales'
+alias bsales='cd ~/projects/sales;yarn run start:sales:dev'
+alias pull='git pull'
+alias push='git push'
+alias cat='bat'
+alias tmux='tmux -f ~/.tmux.conf'
+
+status --is-interactive; and source (rbenv init -|psub)
+
+set -gx OMF_PATH $HOME/.local/share/omf
+set -gx FZF_DEFAULT_COMMAND 'ag -g ""'
+# set fish_theme agnoster
+
+source $OMF_PATH/init.fish
+
+function fish_greeting
+end
+
