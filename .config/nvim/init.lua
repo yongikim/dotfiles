5a5ffb487378b02fddc773f0392e81632b48038d
Yongi Kim <42469083+yongikim@users.noreply.github.com>
2022-09-09 14:02:05 +0900

    Create init.lua

---
+++ .config/nvim/init.lua
@@ -0,0 +1,384 @@
+-- package manager
+require("packer").startup(function(use)
+  use 'wbthomason/packer.nvim'
+
+  -- Fuzzy Finder
+  use {
+    'nvim-telescope/telescope.nvim', tag = '0.1.0',
+    requires = { { 'nvim-lua/plenary.nvim' } }
+  }
+  use { 'kevinhwang91/nvim-hlslens' } -- 単語検索で何個目のヒットかをカーソル横に表示
+
+  -- LSP
+  use 'neovim/nvim-lspconfig'
+  use 'williamboman/mason.nvim'
+  use 'williamboman/mason-lspconfig.nvim'
+
+  -- 補完
+  use "hrsh7th/nvim-cmp"
+  use "hrsh7th/cmp-nvim-lsp"
+  use "hrsh7th/vim-vsnip"
+  use "hrsh7th/cmp-path"
+  use "hrsh7th/cmp-buffer"
+  -- use "hrsh7th/cmp-cmdline"
+  use {
+    "windwp/nvim-autopairs", -- カッコを自動で閉じる
+    config = function() require("nvim-autopairs").setup {} end
+  }
+
+  -- 見た目
+  use {
+    'svrana/neosolarized.nvim',
+    requires = { 'tjdevries/colorbuddy.nvim' }
+  }
+  use "j-hui/fidget.nvim" -- nvim-lspの稼働状況を表示
+  use "kkharji/lspsaga.nvim" -- リッチなUI
+  use 'folke/lsp-colors.nvim' -- 色を豊かに
+  use {
+    'nvim-lualine/lualine.nvim',
+    requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+  }
+  use {
+    "SmiteshP/nvim-navic", -- 今いる関数名をステータスラインに表示
+    requires = "neovim/nvim-lspconfig"
+  }
+  use {
+    'akinsho/bufferline.nvim', -- バッファライン
+    tag = "v2.*",
+    requires = 'kyazdani42/nvim-web-devicons'
+  }
+  use 'onsails/lspkind.nvim' -- 補完の候補の横にアイコンを表示
+  use("petertriho/nvim-scrollbar") -- スクロールバー
+  use {
+    'lewis6991/gitsigns.nvim', -- Git情報を行番号の左に表示
+    config = function()
+      require('gitsigns').setup()
+    end
+  }
+  use { -- エラー情報を表示
+    "folke/trouble.nvim",
+    requires = "kyazdani42/nvim-web-devicons",
+    config = function()
+      require("trouble").setup {
+        -- your configuration comes here
+        -- or leave it empty to use the default settings
+        -- refer to the configuration section below
+      }
+    end
+  }
+
+  -- ターミナル
+  use {
+    "akinsho/toggleterm.nvim", tag = '*',
+    config = function()
+      require("toggleterm").setup {
+        open_mapping = [[<c-\>]],
+        direction = 'float',
+      }
+    end
+  }
+end)
+
+-- 0. Core Settings
+vim.cmd("autocmd!")
+
+vim.g.mapleader = ','
+vim.keymap.set('i', 'jj', '<ESC>')
+
+if vim.loop.os_uname().sysname == 'Darwin' then
+  vim.g.clipboard = {
+    name = 'macOS-clipboard',
+    copy = {
+      ['+'] = 'pbcopy',
+      ['*'] = 'pbcopy',
+    },
+    paste = {
+      ['+'] = 'pbpaste',
+      ['*'] = 'pbpaste',
+    },
+    cache_enabled = 0,
+  }
+  vim.g.python_host_prog = '/usr/bin/python'
+  vim.g.python3_host_prog = '/usr/local/bin/python3'
+end
+
+vim.scriptencoding = 'utf-8'
+vim.opt.encoding = 'utf-8'
+vim.opt.fileencoding = 'utf-8'
+
+vim.wo.number = true
+vim.wo.signcolumn = 'yes'
+
+vim.opt.title = true
+vim.opt.autoindent = true
+vim.opt.smartindent = true
+vim.opt.hlsearch = true
+vim.opt.backup = false
+vim.opt.showcmd = true
+vim.opt.cmdheight = 1
+vim.opt.laststatus = 2
+vim.opt.expandtab = true
+vim.opt.scrolloff = 10
+vim.opt.shell = 'fish'
+vim.opt.backupskip = { '/tmp/*', '/private/tmp/*' }
+vim.opt.inccommand = 'split'
+vim.opt.ignorecase = true -- Case insensitive searching UNLESS /C or capital in search
+vim.opt.smarttab = true
+vim.opt.breakindent = true
+vim.opt.shiftwidth = 2
+vim.opt.tabstop = 2
+vim.opt.wrap = false -- No Wrap lines
+vim.opt.backspace = { 'start', 'eol', 'indent' }
+vim.opt.path:append { '**' } -- Finding files - Search down into subfolders
+vim.opt.wildignore:append { '*/node_modules/*' }
+
+vim.opt.cursorline = true
+vim.opt.termguicolors = true
+vim.opt.winblend = 0
+vim.opt.wildoptions = 'pum'
+vim.opt.pumblend = 5
+vim.opt.background = 'dark'
+
+-- neosolarized
+local status, n = pcall(require, "neosolarized")
+if (not status) then return end
+
+n.setup({
+  comment_italics = true,
+})
+
+local cb = require('colorbuddy.init')
+local Color = cb.Color
+local colors = cb.colors
+local Group = cb.Group
+local groups = cb.groups
+local styles = cb.styles
+
+Color.new('black', '#000000')
+Group.new('CursorLine', colors.none, colors.base03, styles.NONE, colors.base1)
+Group.new('CursorLineNr', colors.yellow, colors.black, styles.NONE, colors.base1)
+Group.new('Visual', colors.none, colors.base03, styles.reverse)
+
+local cError = groups.Error.fg
+local cInfo = groups.Information.fg
+local cWarn = groups.Warning.fg
+local cHint = groups.Hint.fg
+
+Group.new("DiagnosticVirtualTextError", cError, cError:dark():dark():dark():dark(), styles.NONE)
+Group.new("DiagnosticVirtualTextInfo", cInfo, cInfo:dark():dark():dark(), styles.NONE)
+Group.new("DiagnosticVirtualTextWarn", cWarn, cWarn:dark():dark():dark(), styles.NONE)
+Group.new("DiagnosticVirtualTextHint", cHint, cHint:dark():dark():dark(), styles.NONE)
+Group.new("DiagnosticUnderlineError", colors.none, colors.none, styles.undercurl, cError)
+Group.new("DiagnosticUnderlineWarn", colors.none, colors.none, styles.undercurl, cWarn)
+Group.new("DiagnosticUnderlineInfo", colors.none, colors.none, styles.undercurl, cInfo)
+Group.new("DiagnosticUnderlineHint", colors.none, colors.none, styles.undercurl, cHint)
+
+-- Undercurl
+vim.cmd([[let &t_Cs = "\e[4:3m"]])
+vim.cmd([[let &t_Ce = "\e[4:0m"]])
+
+-- Turn off paste mode when leaving insert
+vim.api.nvim_create_autocmd("InsertLeave", {
+  pattern = '*',
+  command = "set nopaste"
+})
+
+-- Add asterisks in block comments
+vim.opt.formatoptions:append { 'r' }
+
+-- 1. LSP Sever management
+require('mason').setup()
+require('mason-lspconfig').setup_handlers({ function(server)
+  local navic = require("nvim-navic")
+  local opt = {
+    -- -- Function executed when the LSP server startup
+    on_attach = function(client, bufnr)
+      local opts = { noremap = true, silent = true }
+      vim.api.nvim_buf_set_keymap(bufnr, 'n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>', opts)
+      vim.cmd 'autocmd BufWritePre * lua vim.lsp.buf.formatting_sync(nil, 1000)'
+      -- navic
+      navic.attach(client, bufnr)
+    end,
+    capabilities = require('cmp_nvim_lsp').update_capabilities(
+      vim.lsp.protocol.make_client_capabilities()
+    )
+  }
+  require('lspconfig')[server].setup(opt)
+end })
+
+-- suppress "undefined global `vim`"
+require 'lspconfig'.sumneko_lua.setup({
+  settings = {
+    Lua = {
+      diagnostics = {
+        globals = { 'vim' },
+      },
+    },
+  },
+})
+
+-- lspkind-nvim
+local lspkind = require('lspkind')
+local cmp = require('cmp')
+local types = require('cmp.types')
+local str = require('cmp.utils.str')
+cmp.setup {
+  formatting = {
+    format = lspkind.cmp_format({
+      mode = 'symbol_text', -- show only symbol annotations
+      maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+
+      -- The function below will be called before any actual modifications from lspkind
+      -- so that you can provide more controls on popup customization. (See [#30](https://github.com/onsails/lspkind-nvim/pull/30))
+      before = function(entry, vim_item)
+        -- Get the full snippet (and only keep first line)
+        local word = entry:get_insert_text()
+        if entry.completion_item.insertTextFormat == types.lsp.InsertTextFormat.Snippet then
+          word = vim.lsp.util.parse_snippet(word)
+        end
+        word = str.oneline(word)
+
+        -- concatenates the string
+        -- local max = 50
+        -- if string.len(word) >= max then
+        -- 	local before = string.sub(word, 1, math.floor((max - 3) / 2))
+        -- 	word = before .. "..."
+        -- end
+
+        if entry.completion_item.insertTextFormat == types.lsp.InsertTextFormat.Snippet
+            and string.sub(vim_item.abbr, -1, -1) == "~"
+        then
+          word = word .. "~"
+        end
+        vim_item.abbr = word
+        return vim_item
+      end
+    })
+  }
+}
+
+require('scrollbar').setup()
+require("scrollbar.handlers.search").setup()
+
+require('lspsaga').setup()
+require('fidget').setup()
+require('lsp-colors').setup()
+local navic = require("nvim-navic")
+require('lualine').setup {
+  options = { theme = 'solarized_dark' },
+  -- navic
+  sections = {
+    lualine_c = {
+      { navic.get_location, cond = navic.is_available },
+    }
+  }
+}
+require("bufferline").setup {}
+-- trouble
+vim.keymap.set("n", "<leader>xx", "<cmd>TroubleToggle<cr>",
+  { silent = true, noremap = true }
+)
+vim.keymap.set("n", "<leader>xw", "<cmd>TroubleToggle workspace_diagnostics<cr>",
+  { silent = true, noremap = true }
+)
+vim.keymap.set("n", "<leader>xd", "<cmd>TroubleToggle document_diagnostics<cr>",
+  { silent = true, noremap = true }
+)
+vim.keymap.set("n", "<leader>xl", "<cmd>TroubleToggle loclist<cr>",
+  { silent = true, noremap = true }
+)
+vim.keymap.set("n", "<leader>xq", "<cmd>TroubleToggle quickfix<cr>",
+  { silent = true, noremap = true }
+)
+vim.keymap.set("n", "gR", "<cmd>TroubleToggle lsp_references<cr>",
+  { silent = true, noremap = true }
+)
+
+-- 2. build-in LSP function
+-- keyboard shortcut
+vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<CR>')
+vim.keymap.set('n', 'gf', '<cmd>lua vim.lsp.buf.formatting()<CR>')
+vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<CR>')
+vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>')
+vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>')
+vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<CR>')
+vim.keymap.set('n', 'gt', '<cmd>lua vim.lsp.buf.type_definition()<CR>')
+vim.keymap.set('n', 'gn', '<cmd>lua vim.lsp.buf.rename()<CR>')
+vim.keymap.set('n', 'ga', '<cmd>lua vim.lsp.buf.code_action()<CR>')
+vim.keymap.set('n', 'ge', '<cmd>lua vim.diagnostic.open_float()<CR>')
+vim.keymap.set('n', 'g]', '<cmd>lua vim.diagnostic.goto_next()<CR>')
+vim.keymap.set('n', 'g[', '<cmd>lua vim.diagnostic.goto_prev()<CR>')
+-- LSP handlers
+vim.lsp.handlers["textDocument/publishDiagnostics"] = vim.lsp.with(
+  vim.lsp.diagnostic.on_publish_diagnostics, { virtual_text = true }-- 表示したくない場合は`false`
+)
+-- Reference highlight
+vim.cmd [[
+set updatetime=500
+highlight LspReferenceText  cterm=underline ctermfg=1 ctermbg=8 gui=underline guifg=#A00000 guibg=#104040
+highlight LspReferenceRead  cterm=underline ctermfg=1 ctermbg=8 gui=underline guifg=#A00000 guibg=#104040
+highlight LspReferenceWrite cterm=underline ctermfg=1 ctermbg=8 gui=underline guifg=#A00000 guibg=#104040
+augroup lsp_document_highlight
+  autocmd!
+  autocmd CursorHold,CursorHoldI * lua vim.lsp.buf.document_highlight()
+  autocmd CursorMoved,CursorMovedI * lua vim.lsp.buf.clear_references()
+augroup END
+]]
+
+-- 3. completion (hrsh7th/nvim-cmp)
+cmp.setup({
+  snippet = {
+    expand = function(args)
+      vim.fn["vsnip#anonymous"](args.body)
+    end,
+  },
+  sources = {
+    { name = "nvim_lsp" },
+    { name = "buffer" },
+    { name = "path" },
+  },
+  mapping = cmp.mapping.preset.insert({
+    ["<C-p>"] = cmp.mapping.select_prev_item(),
+    ["<C-n>"] = cmp.mapping.select_next_item(),
+    ['<C-l>'] = cmp.mapping.complete(),
+    ['<C-e>'] = cmp.mapping.abort(),
+    ["<CR>"] = cmp.mapping.confirm { select = true },
+  }),
+  experimental = {
+    ghost_text = true,
+  },
+})
+-- cmp.setup.cmdline('/', {
+--   mapping = cmp.mapping.preset.cmdline(),
+--   sources = {
+--     { name = 'buffer' }
+--   }
+-- })
+-- cmp.setup.cmdline(":", {
+--   mapping = cmp.mapping.preset.cmdline(),
+--   sources = {
+--     { name = "path" },
+--     { name = "cmdline" },
+--   },
+-- })
+require('nvim-autopairs').setup()
+
+-- telescope
+require('telescope').setup()
+vim.keymap.set('n', '<leader>f', '<cmd>Telescope find_files<cr>')
+vim.keymap.set('n', '<leader>g', '<cmd>Telescope live_grep<cr>')
+vim.keymap.set('n', '<leader>b', '<cmd>Telescope buffers<cr>')
+vim.keymap.set('n', '<leader>h', '<cmd>Telescope help_tags<cr>')
+vim.keymap.set('n', '<leader>r', '<cmd>Telescope oldfiles<cr>')
+
+-- hlslens
+local kopts = { noremap = true, silent = true }
+vim.api.nvim_set_keymap('n', 'n',
+  [[<Cmd>execute('normal! ' . v:count1 . 'n')<CR><Cmd>lua require('hlslens').start()<CR>]], kopts)
+vim.api.nvim_set_keymap('n', 'N',
+  [[<Cmd>execute('normal! ' . v:count1 . 'N')<CR><Cmd>lua require('hlslens').start()<CR>]], kopts)
+vim.api.nvim_set_keymap('n', '*', [[*<Cmd>lua require('hlslens').start()<CR>]], kopts)
+vim.api.nvim_set_keymap('n', '#', [[#<Cmd>lua require('hlslens').start()<CR>]], kopts)
+vim.api.nvim_set_keymap('n', 'g*', [[g*<Cmd>lua require('hlslens').start()<CR>]], kopts)
+vim.api.nvim_set_keymap('n', 'g#', [[g#<Cmd>lua require('hlslens').start()<CR>]], kopts)
+vim.api.nvim_set_keymap('n', '<Leader>l', ':noh<CR>', kopts)
